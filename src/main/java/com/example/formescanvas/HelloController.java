abcf101f7ff0325a97f23a2ec646e5e92975d63f
Nadine Turpin <devanalyste@gmail.com>
2021-09-15 13:07:55 -0400

    Initial commit

---
+++ src/main/java/com/example/formescanvas/HelloController.java
@@ -0,0 +1,35 @@
+package com.example.formescanvas;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML private Canvas drawingCanvas;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        GraphicsContext gc = drawingCanvas.getGraphicsContext2D();
+    }
+}
